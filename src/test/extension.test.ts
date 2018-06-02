c10a3f9fdd1461583cb327760d0910c027492e1a
David Marek <david@marek.me>
2018-06-02 21:07:15 +0200

    Fixed quotes

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -4,16 +4,16 @@ import { assert } from 'chai';
 import * as vscode from 'vscode';
 import * as myExtension from '../extension';
 
-describe("Extension", function () {
-    it("should export activate function", function () {
-        assert.typeOf(myExtension.activate, "function");
+describe('Extension', function () {
+    it('should export activate function', function () {
+        assert.typeOf(myExtension.activate, 'function');
     });
 
-    it("should export deactivate function", function () {
-        assert.typeOf(myExtension.deactivate, "function");
+    it('should export deactivate function', function () {
+        assert.typeOf(myExtension.deactivate, 'function');
     });
 
-    it("should activate when command is executed", async function () {
+    it('should activate when command is executed', async function () {
         await vscode.commands.executeCommand('jsonPathExtract.queryToPlainText');
         const list = await vscode.commands.getCommands();
         assert.include(list, 'jsonPathExtract.queryToPlainText');
